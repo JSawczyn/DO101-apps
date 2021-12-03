09440b2ed42fae81c4b857748c30b903a114d114
jsawczy <josephsawczyn@discover.com>
2021-12-03 09:59:53 -0500

    output

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/mars', function(req, res) {
-   res.send('Hello Mars!\n');
+   res.send('Hello Mars Sawczyn!\n');
 });
 
 app.listen(8080, function () {
